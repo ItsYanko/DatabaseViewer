54579da6021fd61506a9d4c99055ce90cdf68f5d
ItsYanko <ianperina@gmail.com>
2021-03-09 01:24:38 +0000

    add table design, main menu retouches, add fonts, add navigation in table

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 500,
+        minHeight: 300,
+        maxWidth: 1000,
+        maxHeight: 750,
+        title: "Base de Dados do ZOO",
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  win.loadFile('./app/index.html')
+    win.loadFile('./app/index.html');
+    //win.setMenu(null);
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 })
